f6b7528837dd1bb5ac6df97c7206e57c9d9255cb
Luke Hammond <luke.john.hammond@gmail.com>
2017-01-06 20:50:18 +0000

    Added script start

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.use(express.static(`${__dirname}/public`));
 app.use(router);
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
+
+
+//mongodb://heroku_jvtchd6h:37f3pga71gnpa48ihh5mn3hude@ds157158.mlab.com:57158/heroku_jvtchd6h
