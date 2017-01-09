5331f0f051f52e1eaa4658f90735b1d50fe2ec98
Luke Hammond <luke.john.hammond@gmail.com>
2017-01-09 06:42:58 +0000

    Added most of framework for authentication for cms, ready to assign secure routes

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const router = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const secret    = require('../config/token').secret;
+const jwt       = require('jsonwebtoken');
 
 const authController = require('../controllers/authController');
 const blogsController = require('../controllers/blogsController');
+
+// middleware
+
+function secureRoute(req, res, next) {
+  if(!req.headers.authorization) return res.status(401).json({ message: 'Invalid credentials, please try again' });
+
+  const token = req.headers.authorization.replace('Bearer ', '');
+
+  jwt.verify(token, secret, (err, payload) => {
+    if(err) return res.status(401).json({ message: "Invalid credentials, please try again."});
+    req.user = payload;
+
+    next();
+  });
+}
+
+
 
 router.route('/register')
   .post(authController.register);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const validator = require("validator");
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
