5bd4f441ecd72114b49ce835aa70c162035f3cc6
Luke Hammond <luke.john.hammond@gmail.com>
2017-01-11 17:55:44 +0000

    Added FileReader() to convert to base64

--- src/admin/js/directives/dragDrop.js
+++ src/admin/js/directives/dragDrop.js
@@ -2,11 +2,17 @@ angular.module('admin')
   .directive('dragdrop', dragdrop);
 
 function dragdrop() {
+  const reader = new FileReader();
   return {
     restrict: 'E',
     replace: true,
     templateUrl: 'templates/dragDrop.html',
     link($scope, element) {
+
+      reader.onload = () => {
+        console.log(reader.result);
+      };
+
       element
         .on('dragover', (e) => {
           e.preventDefault();
@@ -18,6 +24,8 @@ function dragdrop() {
 
           const files = (e.target.files || e.dataTransfer.files)[0];
           console.log(files);
+
+          reader.readAsDataURL(files);
         });
     }
   };
