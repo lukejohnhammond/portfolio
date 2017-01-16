be3755cfe4c7c1ad56b2dce6c89be5849878f9f8
Luke Hammond <luke.john.hammond@gmail.com>
2017-01-16 10:20:03 +0000

    Added extra fields to work model

--- models/work.js
+++ models/work.js
@@ -4,8 +4,10 @@ const workSchema = new mongoose.Schema({
   title: { type: String, required: true },
   shortIntro: { type: String, required: true },
   mainContent: { type: String },
+  mainContentSecondary: { type: String },
+  mainContentTertiary: { type: String },
   heroImage: { type: String },
-  images: { type: String },
+  images: [{ type: String }],
   gitHub: { type: String },
   liveLink: { type: String }
 }, {
